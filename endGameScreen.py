789c04be798b0e42d59a93dc303298eb768deadd
Nikolai <nbaklanov@issart.com>
2020-01-20 00:08:17 +0600

    Каркас экранов, навигация между ними.

---
+++ endGameScreen.py
@@ -0,0 +1,30 @@
+import pygame
+
+class EndScreen:
+    myfont = None
+    screenTitle = None
+    pygameLib = None
+    window = None
+    screenSize = None
+
+    def __init__(self, pygameInstance, windowScreen):
+        self.pygameLib = pygameInstance
+        self.window = windowScreen
+        self.screenSize = self.window.get_size()
+
+        self.myfont = self.pygameLib.font.SysFont('Comic Sans MS', 30)
+        self.screenTitle = self.myfont.render('End Screen', False, (255, 0, 0))
+
+    def draw(self, eventsList):
+        returnValue = 0
+
+        for currentEvent in eventsList:
+            if currentEvent.type == self.pygameLib.KEYDOWN:
+                if currentEvent.key == self.pygameLib.K_LEFT:
+                    returnValue = 2
+                elif currentEvent.key == self.pygameLib.K_RIGHT:
+                    returnValue = 1
+
+        self.window.blit(self.screenTitle, (0, 0))
+
+        return returnValue
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+from mainScreen import MainScreen
+from gameScreen import GameScreen
+from endGameScreen import EndScreen
 
 MAIN_WINDOW_WIDTH = 600
 MAIN_WINDOW_HEIGHT = 400
@@ -7,9 +10,11 @@ BACKGROUND_COLOR = (0, 0, 0)
 
 pygame.init()
 
+drawResult = 0
 gameIsOn = True
 clock = pygame.time.Clock()
 screenSurface = pygame.display.set_mode((MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT))
+currentScreen = MainScreen(pygame, screenSurface)
 
 while gameIsOn == True:
 
@@ -19,6 +24,15 @@ while gameIsOn == True:
             gameIsOn = False
 
     screenSurface.fill(BACKGROUND_COLOR)
+
+    drawResult = currentScreen.draw(systemEvents)
+    if drawResult == 1:
+        currentScreen = MainScreen(pygame, screenSurface)
+    elif drawResult == 2:
+        currentScreen = GameScreen(pygame, screenSurface)
+    elif drawResult == 3:
+        currentScreen = EndScreen(pygame, screenSurface)
+
     pygame.display.flip()
     clock.tick(60)
 
